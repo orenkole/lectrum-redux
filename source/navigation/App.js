10efe45f62f6e32f33c215206e98a3a39f2ce044
badger <badger@badger.local>
2022-05-24 09:19:43 +0300

    57. Add routing

--- source/navigation/App.js
+++ source/navigation/App.js
@@ -1,17 +1,24 @@
 // Core
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
+import {Redirect, Route, Switch} from "react-router-dom";
 
 // Pages
-import { Feed } from '../pages';
+import {Feed, Login, NewPassword, Profile, Signup} from '../pages';
+import {book} from "./book";
 
 @hot(module)
 export default class App extends Component {
     render () {
         return (
-            <>
-                <Feed />
-            </>
+            <Switch>
+                <Route path={book.login} component={Login} />
+                <Route path={book.signUp} component={Signup} />
+                <Route path={book.feed} component={Feed} />
+                <Route path={book.profile} component={Profile} />
+                <Route path={book.newPassword} component={NewPassword} />
+                <Redirect to={book.login} />
+            </Switch>
         );
     }
 }
