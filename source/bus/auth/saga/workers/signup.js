3780236f85359a2f7a8d566ee59958c753a6962c
badger <badger@badger.local>
2022-05-28 14:04:59 +0300

    60. signup worker saga

---
+++ source/bus/auth/saga/workers/signup.js
@@ -0,0 +1,22 @@
+import {apply, put} from "redux-saga/effects";
+import {uiAction} from "../../../ui/actions";
+import {authAction} from "../../actions";
+import {api} from "../../../../REST";
+
+
+export function* signup({payload: userInfo}) {
+    try {
+        yield put(uiAction.startFetching());
+        const response = yield apply(api, api.auth.signup, [userInfo])
+        const {data: profile, message} = yield apply(response, response.json);
+        if (response.status !== 200) {
+            throw new Error(message);
+        }
+        console.log({profile})
+        yield put(authAction.authenticate())
+    } catch(error) {
+        yield put(uiAction.emitError(error, 'signup workder'))
+    } finally {
+        yield put(uiAction.startFetching())
+    }
+}
