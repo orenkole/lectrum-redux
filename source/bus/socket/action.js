e947741d0461880c8861014a838afe2e99ce5c80
badger <badger@badger.local>
2022-06-04 12:55:34 +0300

    81. like and unlike websocket

--- source/bus/socket/action.js
+++ source/bus/socket/action.js
@@ -11,10 +11,27 @@ export const socketActions = {
             dispatch(uiAction.setOfflineState());
         })
     },
-    listenPosts: () => dispatch => {
+    listenPosts: () => (dispatch, getState) => {
         socket.on('create', (event) => {
             const {data: post} = JSON.parse(event);
             dispatch(postsActions.createPost(post))
+        });
+
+        socket.on('like', (event) => {
+            const {data, meta} = JSON.parse(event);
+            if (meta.action === 'like'); {
+                const liker = getState()
+                    .users.find(user => user.get('id') === data?.userId);
+
+                dispatch(
+                    postsActions.likePost({
+                        postId: data.postId,
+                        liker
+                    })
+                )
+            } else {
+                dispatch(postsActions.unlikePost(data))
+            }
         })
-    }
+    },
 }
